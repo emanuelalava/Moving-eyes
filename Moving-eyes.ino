6e1b187e0afb5fde7d41223947f0af298840a35c
Winter Alava <walava@espol.edu.ec>
2020-07-04 02:20:50 -0500

    Se anadio la funcion Up,Down y Center para mover a un servo a dichas posiciones

--- Moving-eyes.ino
+++ Moving-eyes.ino
@@ -1,9 +1,75 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+Servo servo;
+const int PIN_SERVO = 11;
+const int SERIAL_BAUD = 9600;
+const int DELAY = 25;
+void setup() {
+  Serial.begin(SERIAL_BAUD);
+  servo.attach(PIN_SERVO);
+  servo.write(90);
+  delay(2000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  String command = Serial.readString();// s1 is String type variable.
+  Serial.print(command.indexOf("MOVER") >= 0);
+  Serial.print("Received Data => ");
+  Serial.println(command);
+  if (command.indexOf("MOVER") >= 0) {
+    Serial.println("Moving...");
+    Up(servo);
+    Down(servo);
+    Center(servo);
+  }
+  delay(100);
+}
+
+
+void Down(Servo servoMotor) {
+  int posActual = servoMotor.read();
+  for (int i = posActual; i < 135; i++) {
+    if ((servoMotor.read() >= 0) && (servoMotor.read() <= 180)) {
+      servoMotor.write(i);
+    }
+    delay(DELAY);
+  }
+  Serial.print("END OF DOWN");
+}
+
+void Up(Servo servoMotor) {
+  int posActual = servoMotor.read();
+  for (int i = posActual; i > 45; i--) {
+    if ((servoMotor.read() >= 0) && (servoMotor.read() <= 180)) {
+      servoMotor.write(i);
+    }
+    delay(DELAY);
+  }
+  Serial.print("END OF UP");
+
+}
+
+
+void Center(Servo servoMotor) {
+  int posActual = servoMotor.read();
+  Serial.print("Center");
+  Serial.print(posActual);
+  if (posActual > 90) {
+
+    for (int i = posActual; i > 90; i--) {
+      if ((servoMotor.read() >= 0) && (servoMotor.read() <= 180)) {
+        servoMotor.write(i);
+      }
+      delay(DELAY);
+    }
+  } else {
+    for (int i = posActual; i < 90; i++) {
+      if ((servoMotor.read() >= 0) && (servoMotor.read() <= 180)) {
+        servoMotor.write(i);
+      }
+      delay(DELAY);
+    }
+  }
+  Serial.print("END OF CENTER");
 
 }
